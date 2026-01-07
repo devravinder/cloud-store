4da065fd855b2f9ba9342fd76490bd908aa795b1
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2026-01-07 17:21:19 +0530

    fix: icons

--- web-app/src/Header.tsx
+++ web-app/src/Header.tsx
@@ -21,14 +21,14 @@ export default function Header({
         <button
           onClick={onRefresh}
           title="Refresh"
-          className=" text-blue-500 cursor-pointer inline-flex items-center px-3 py-2 bg-slate-100 text-lg font-bold rounded-lg hover:bg-slate-200 "
+          className=" text-blue-500 cursor-pointer inline-flex items-center px-2 py-1 bg-slate-100 text-2xl font-bold rounded-lg hover:bg-slate-200 "
         >
           {REFRESH}
         </button>
         <button
           onClick={logout}
           title="Logout"
-          className=" text-blue-500 cursor-pointer inline-flex items-center px-3 py-2 bg-slate-100 text-lg font-bold rounded-lg hover:bg-slate-200 "
+          className=" text-blue-500 cursor-pointer inline-flex items-center px-2 py-1 bg-slate-100 text-lg font-bold rounded-lg hover:bg-slate-200 "
         >
           {LOGOUT}
         </button>
