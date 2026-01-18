239529f50d4e9ea90ba3217cb8142fe81562b07b
Ravinder Reddy Kothabad <developer.ravinder.reddy@gmail.com>
2026-01-19 03:13:58 +0530

    fix: move json middle after routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,17 +12,18 @@ import { authenticate } from './authMiddleware.js';
 const app: Express = express();
 app.use(cors());
 
-app.use(express.json({ limit: '15mb' }));
-app.use(express.urlencoded({ 
-  extended: true, 
-  limit: '15mb',
-  parameterLimit: 50000 
-}));
+
 
 app.get("/test",(req, res)=>res.json("Hello World"))
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(API_PREFIX, authenticate, routes, clientErrorHandler, serverErrorHandler);
 
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ 
+  extended: true, 
+  limit: '15mb',
+  parameterLimit: 50000 
+}));
 
 export default app
